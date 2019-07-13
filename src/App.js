4caef9b0b921f3984df22502df3240f9483aadc5
DongMing <hdmdhr@gmail.com>
2019-07-12 21:44:02 -0600

    8.151-152: Create Layout component. | Aux, BurgerBuilder component. | Install node-sass.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import Layout from './components/layout/Layout'
+import BurgerBuilder from './containers/burger-builder/BurgerBuilder'
+
 
 function App() {
   return (
     <div>
-     
+      <Layout>
+        <BurgerBuilder />
+      </Layout>
     </div>
   );
 }

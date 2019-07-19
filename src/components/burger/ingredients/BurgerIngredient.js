f7ba07b2f8b15f36897f2a97af4c17d84d8bd454
DongMing <hdmdhr@gmail.com>
2019-07-18 20:27:25 -0600

    8.153: Download BurgerIngredient.css, create BurgerIngredient component to switch and implement styles.

---
+++ src/components/burger/ingredients/BurgerIngredient.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import styles from './BurgerIngredient.module.css'
+
+const BurgerIngredient = props => {
+    let ingredient = null;
+
+    switch (props.type) {
+        case 'bread-bottom':
+            ingredient = <div className={styles.BreadBottom}></div>
+            break;
+        case 'bread-top':
+            ingredient = (
+            <div className={styles.BreadTop}>
+                <div className={styles.Seed1} />
+                <div className={styles.Seed2} />
+            </div>)
+            break;
+        case 'meat':
+                ingredient = <div className={styles.Meat}></div>
+                break;
+        case 'cheese':
+                ingredient = <div className={styles.Cheese}></div>
+                break;
+        case 'bacon':
+                ingredient = <div className={styles.Bacon}></div>
+                break;
+        case 'salad':
+                ingredient = <div className={styles.Salad}></div>
+                break; 
+        default:
+            break;
+    }
+
+    return ingredient;
+};
+
+export default BurgerIngredient;

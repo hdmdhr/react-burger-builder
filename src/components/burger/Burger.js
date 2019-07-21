f85ab86e42728125d67a79f23484bd8e4d8287ce
Dongming(Daniel) Hu <daniel@vogcalgaryappdeveloper.com>
2019-07-20 20:16:24 -0600

    8.156: Add state to BurgerBuilder. | Use super complicated map method to map state.ingredients into BurgerIngredient component array.
    Elements in an array of component need a key.

--- src/components/burger/Burger.js
+++ src/components/burger/Burger.js
@@ -2,16 +2,23 @@ import React from 'react';
 import styles from './Burger.module.scss';
 import BurgerIngredient from './ingredients/BurgerIngredient'
 
-const Burger = props =>(
-<div className={styles.Burger}>
-        <BurgerIngredient type="bread-top" />
-        <BurgerIngredient type="cheese" />
-        <BurgerIngredient type="meat" />
-        <BurgerIngredient type="salad" />
-        <BurgerIngredient type="bread-bottom" />
-</div>
-)
+const Burger = props => {
+        // MAP INGREDIENTS INTO COMPONENTS ARRAY (compo array need keys)
+        const transformedIngredients = Object.keys(props.ingredients)
+                .map( key => 
+                        [...Array(props.ingredients[key])].map( (_, index) => {
+                                return <BurgerIngredient key={key + index} type={key} />
+                        })
+                )
 
-;
+        return (
+                <div className={styles.Burger}>
+                        <BurgerIngredient type="bread-top" />
+                        { transformedIngredients }
+                        <BurgerIngredient type="bread-bottom" />
+                </div>
+        )
+
+};
 
 export default Burger;

938b81305a53687b707c01575dafb5da1851ae8c
Dongming(Daniel) Hu <daniel@vogcalgaryappdeveloper.com>
2019-07-28 14:26:54 -0600

    8.161: Enable remove ingredient. | Disable less button when a certain ingredient is less than 0.

--- src/containers/burger-builder/BurgerBuilder.js
+++ src/containers/burger-builder/BurgerBuilder.js
@@ -27,22 +27,36 @@ class BurgerBuilder extends Component {
     addIngredientHandler = type => {
         const oldCount = this.state.ingredients[type]
         const newCount = oldCount + 1
-        const updatedIngredients = {...this.state.ingredients}
-        updatedIngredients[type] = newCount
+        const clonedIngredients = {...this.state.ingredients}
+        clonedIngredients[type] = newCount
         const priceAddition = INGREDIENT_PRICES[type]
         const newPrice = this.state.totalPrice + priceAddition
-        this.setState({ totalPrice: newPrice, ingredients: updatedIngredients })
+        this.setState({ totalPrice: newPrice, ingredients: clonedIngredients })
     }
 
     removeIngredientHandler = type => {
-
+        if (this.state.ingredients[type] <= 0) return
+        const newCount = this.state.ingredients[type] - 1
+        const clonedIngredients = {...this.state.ingredients}
+        clonedIngredients[type] = newCount
+        const priceSubtraction = INGREDIENT_PRICES[type]
+        const newPrice = this.state.totalPrice - priceSubtraction
+        this.setState({ totalPrice: newPrice, ingredients: clonedIngredients })
     }
 
     render () {
+        const ingredientsLess0 = {...this.state.ingredients}
+        for (let key in ingredientsLess0) {
+            ingredientsLess0[key] = ingredientsLess0[key] <= 0
+        }
+        console.log(ingredientsLess0);
+        
         return (
             <Aux>
                 <Burger ingredients={this.state.ingredients}/>
-                <BuildControls ingredientAdded={this.addIngredientHandler} />
+                <BuildControls ingredientAdded={this.addIngredientHandler}
+                    ingredientRemoved={this.removeIngredientHandler}
+                    disabled={ingredientsLess0} />
             </Aux>
         );
     }
